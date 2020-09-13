9acc58de83e6f069daf7f98fd9964d17a83a5fca
Praharsh B <praharshdec8@gmail.com>
2020-09-12 17:31:37 -0700

    test vm ip upd

--- src/test/java/servlet/viewticket.java
+++ src/test/java/servlet/viewticket.java
@@ -16,7 +16,7 @@ public class viewticket extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://104.199.125.242:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://34.83.201.249:5432/postgres";
 			String username="postgres";
 			String password="password";
 						
